46edf652e442803a1534bc23ba73a516df6e5466
Douglas Urner <dlu@canishe.com>
2020-01-01 08:30:56 -0800

    Fix shbang, add TODOs.

--- tools/roster.rb
+++ tools/roster.rb
@@ -1,7 +1,12 @@
-#! ruby
+#!/usr/bin/env ruby
 
 # Roster driver - given a CSV file of student demographics, generate
 # the initial JSON to populate the tiles.
+
+# TODO: normalize name presentation.
+# TODO: split given and family name.
+# TODO: map to preferred name.
+# TODO: pronouns?
 
 require 'csv'
 
