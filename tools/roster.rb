3ae5f53ad5a36a0d6be777a2c3e55cf1d1d07024
Douglas Urner <dlu@canishe.com>
2020-01-01 12:25:46 -0800

    Start name normalization.

--- tools/roster.rb
+++ tools/roster.rb
@@ -14,6 +14,22 @@ def kv_pair(k, v)
   print "\t\t\"" + k + "\": \"" + v + "\"\n"
 end
 
+def clean_name(n)
+  # 0: preficx
+  # 1: given name
+  # 2: middle name
+  # 3: family name
+  # 4: suffix
+  parts = Array.new
+  tmp1 = n.split(/,\s*/)
+  parts[3] = tmp1[0]
+  tmp1[1].strip!
+  tmp2 = tmp1[1].split(/\s\s*/)
+  parts[1] = tmp2[0]
+  parts[2] = tmp2[1]
+  parts
+end
+
 first_time = true
 
 print "{\n\"students\": ["
@@ -25,8 +41,12 @@ students.each { |student|
   else
     print ","
   end
+
+  names = clean_name(student[0])
+
   print "{\n"
-  kv_pair("name", student[0])
+  kv_pair("fn", names[1])
+  kv_pair("ln", names[3])
   kv_pair("GHuser", "")
   print "\t}"
 }
