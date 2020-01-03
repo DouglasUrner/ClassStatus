513b45916c4563f3efa060063dac3ec1a2685bbd
Douglas Urner <dlu@canishe.com>
2020-01-03 14:35:57 -0800

    Add GUID to Student model

---
+++ classstatus/db/migrate/20200103223220_add_guid_to_student.rb
@@ -0,0 +1,5 @@
+class AddGuidToStudent < ActiveRecord::Migration[6.0]
+  def change
+    add_column :students, :guid, :integer
+  end
+end
--- classstatus/db/schema.rb
+++ classstatus/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_011129) do
+ActiveRecord::Schema.define(version: 2020_01_03_223220) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_01_02_011129) do
     t.string "github_user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "guid"
   end
 
   add_foreign_key "sections", "courses"
