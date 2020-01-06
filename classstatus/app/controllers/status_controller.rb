328fc494c21096aa99dceb5f346d6b5776d6c147
Douglas Urner <dlu@canishe.com>
2020-01-06 05:23:56 -0800

    Use show action instead of list.
    Defaulting actice section isn't working.

--- classstatus/app/controllers/status_controller.rb
+++ classstatus/app/controllers/status_controller.rb
@@ -1,10 +1,14 @@
 class StatusController < ApplicationController
   helper StudentsHelper
 
-  def index
-    @enrollments = Enrollment.where(section_id: params[:id])
-  end
+  @active_section = 1
 
   def show
+    if (params[:id] == nil)
+      params[:id] = @active_section
+    end
+    @enrollments = Enrollment.where(section_id: params[:id])
+    @active_section = params[:id].to_i
   end
+
 end
--- classstatus/config/routes.rb
+++ classstatus/config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file,
   # see https://guides.rubyonrails.org/routing.html
 
-  get 'status/index'
-  get 'status/:id', to: 'status#index'
+  get 'status/show'
+  # get 'status/:id', to: 'status#show'
+  # get 'status/show/:id', to: 'status#show'
 
-  root 'status#index'
+  root 'status#show'
 
+  resources :status
   resources :courses
   resources :sections
   resources :students do
