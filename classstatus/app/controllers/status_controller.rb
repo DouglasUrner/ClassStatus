7aad2067adf19a6e654426bda6263cd492db3c29
Douglas Urner <dlu@canishe.com>
2020-01-06 21:23:38 -0800

    Avoid crash when @students is empty.

--- classstatus/app/controllers/status_controller.rb
+++ classstatus/app/controllers/status_controller.rb
@@ -2,7 +2,15 @@ class StatusController < ApplicationController
   helper StudentsHelper
 
   def show
+    if (@Student == nil)
+      # First we need students, so when the students table is empty
+      # go to the import page.
+      redirect_to students_path
+    end
     if (params[:id] == nil)
+      # If the block is not specified, make a guess.
+      # For now, the last block viewed or block 1.
+      # TODO: infer from schedule.
       params[:id] = session[:active_block] || 1
     end
     @enrollments = Enrollment.where(section_id: params[:id])
