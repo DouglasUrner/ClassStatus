ddf0ced1ad0dd9b8238c3d4031b366271e7b5991
Douglas Urner <dlu@canishe.com>
2020-01-08 10:36:44 -0800

    Helper for block selection.

--- classstatus/app/helpers/status_helper.rb
+++ classstatus/app/helpers/status_helper.rb
@@ -1,2 +1,17 @@
 module StatusHelper
+  # Display links to the sections passed in s during year y.
+  def block_selector(y, s)
+    links = ""
+    (1..8).each do |b|
+      if (Section.find_by(semester: s, block: b) == nil) then next end
+      if (b == params[:id].to_i)
+        links += link_to b, status_path(b), class: "active"
+      else
+        links += link_to b, status_path(b)
+      end
+      links += " "
+    end
+    return links.html_safe
+  end
+
 end
