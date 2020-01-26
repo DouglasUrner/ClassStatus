4a992a76a25faa8460336d085b4df6ce5699184b
Douglas Urner <dlu@canishe.com>
2020-01-26 08:55:27 -0800

    Section selection dropdown and helper

--- classstatus/app/helpers/progress_helper.rb
+++ classstatus/app/helpers/progress_helper.rb
@@ -1,2 +1,10 @@
 module ProgressHelper
+  def progress_section_picker
+    items = ""
+    # XXX: sorting on :block_id is fragile - use block.sort_order
+    Section.order(:block_id).each do |s|
+      items += "<li>#{link_to s.short_name, progress_path(s), class: 'dropdown-item'}</li>"
+    end
+    items.html_safe
+  end
 end
