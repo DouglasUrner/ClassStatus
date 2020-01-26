7c7f58bf7fef93d31fb7eab225d7f58b3feaf50f
Douglas Urner <dlu@canishe.com>
2020-01-25 21:31:00 -0800

    Name method for Term

--- classstatus/app/models/term.rb
+++ classstatus/app/models/term.rb
@@ -3,4 +3,12 @@ class Term < ApplicationRecord
   belongs_to :term_name
   validates :start_date, presence: true
   validates :end_date,   presence: true
+
+  def name
+    term_name.name
+  end
+
+  def short_name
+    term_name.short_name
+  end
 end
