471b0c4cb555eb1970facbbe935e6d8fb40c27cf
Douglas Urner <dlu@canishe.com>
2020-01-26 08:51:21 -0800

    Trying to get jQuery working

--- classstatus/app/javascript/packs/application.js
+++ classstatus/app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,6 +20,25 @@ import 'bootstrap'
 import '../stylesheets/application'
 import '@fortawesome/fontawesome-free/css/all.css';
 
+// TODO: Figure out what should really be here - all of this code is from
+//       tutorials that are either testing that jQuery has loaded or examples
+//       of how to do a small task in isolation.
+//
+//       For whatever it is worth jQuery here runs, jQuery on .html.erb pages
+//       does not.
+
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).on('turbolinks:load', function () {
+})
+
+// For navbar - change dropdown button to reflect selected choice.
+$(document).ready(function () {
+  $("#navbar-current-section").click(function(){
+    $(this).parents(".dropdown").find('.btn').html($(this).text() +
+     ' <span class="caret"></span>');
+    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+  });
+});
