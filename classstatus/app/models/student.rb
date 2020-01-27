408243a13b2d78500aa07cbdd579ea320d9cd023
Douglas Urner <dlu@canishe.com>
2020-01-26 22:40:58 -0800

    List sections in student list view

--- classstatus/app/models/student.rb
+++ classstatus/app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   has_many :enrollments, dependent: :destroy
   has_many :sections, through: :enrollments
-  
+
   validates :given_name,  presence: true
   validates :family_name, presence: true
 
@@ -15,5 +15,13 @@ class Student < ApplicationRecord
 
   def sortable_name
     "#{family_name}, #{preferred_name ? preferred_name : given_name}"
+  end
+
+  def section_list
+    list = ""
+    self.sections.each do |s|
+      list += "#{s.block.name} "
+    end
+    list.strip
   end
 end
