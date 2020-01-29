679a902ee689f28ace5fd0d9cdfcd70f2cf05541
Douglas Urner <dlu@canishe.com>
2020-01-28 19:03:11 -0800

    Move progress (et al) to section_controller

--- classstatus/app/controllers/sections_controller.rb
+++ classstatus/app/controllers/sections_controller.rb
@@ -1,5 +1,11 @@
 class SectionsController < ApplicationController
-  before_action :set_section, only: [:show, :edit, :update, :destroy]
+  before_action :set_section,
+    only: [:show, :edit, :update, :destroy,
+      :seating, :attendance, :progress]
+  before_action :set_enrollments,
+    only: [:show, :seating, :attendance, :progress]
+
+  helper SeatmapHelper
 
   # GET /sections
   # GET /sections.json
@@ -12,7 +18,7 @@ class SectionsController < ApplicationController
   # GET /sections/1
   # GET /sections/1.json
   def show
-    @enrollments = Enrollment.where(section_id: @section)
+    #@enrollments = Enrollment.where(section_id: @section)
   end
 
   # GET /sections/new
@@ -31,11 +37,14 @@ class SectionsController < ApplicationController
 
     respond_to do |format|
       if @section.save
-        format.html { redirect_to @section, notice: 'Section was successfully created.' }
-        format.json { render :show, status: :created, location: @section }
+        format.html { redirect_to @section,
+          notice: 'Section was successfully created.' }
+        format.json { render :show,
+          status: :created, location: @section }
       else
         format.html { render :new }
-        format.json { render json: @section.errors, status: :unprocessable_entity }
+        format.json { render json: @section.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -45,11 +54,13 @@ class SectionsController < ApplicationController
   def update
     respond_to do |format|
       if @section.update(section_params)
-        format.html { redirect_to @section, notice: 'Section was successfully updated.' }
+        format.html { redirect_to @section,
+          notice: 'Section was successfully updated.' }
         format.json { render :show, status: :ok, location: @section }
       else
         format.html { render :edit }
-        format.json { render json: @section.errors, status: :unprocessable_entity }
+        format.json { render json: @section.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -59,18 +70,41 @@ class SectionsController < ApplicationController
   def destroy
     @section.destroy
     respond_to do |format|
-      format.html { redirect_to sections_url, notice: 'Section was successfully destroyed.' }
+      format.html { redirect_to sections_url,
+        notice: 'Section was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  # /sections/1/seating
+  def seating
+  end
+
+  def attendance
+  end
+
+  def progress
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_section
-      @section = Section.find(params[:id])
+      if (params[:id])
+        @section = Section.find(params[:id])
+      else
+        # TODO: preserve the section when it is specified so that it can
+        #       be defaulted. We might try to default the section based on
+        #       the current time.
+        @section = Section.find(1)
+      end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_enrollments
+      @enrollments = Enrollment.where(section_id: params[:id])
+    end
+
+    # Never trust parameters from the scary internet,
+    # only allow the white list through.
     def section_params
       params.require(:section).permit(:course_id, :year_id, :term_id, :block_id)
     end
--- classstatus/config/routes.rb
+++ classstatus/config/routes.rb
@@ -3,18 +3,21 @@ Rails.application.routes.draw do
   resources :enrollments
   devise_for :users
 
-  resources :sections
   resources :blocks
   resources :courses
+  resources :progress
+  resources :sections
+  resources :students do
+    collection { post :import }
+  end
   resources :terms
   resources :term_names
   resources :years
 
   #get 'progress/:id', to: 'progress#show'
-  resources :progress
-  resources :students do
-    collection { post :import }
-  end
+  get 'sections/:id/attendance', to: 'sections#attendance'
+  get 'sections/:id/progress',   to: 'sections#progress'
+  get 'sections/:id/seating',    to: 'sections#seating'
 
-  root to: 'progress#show'
+  root to: 'sections#progress'
 end
