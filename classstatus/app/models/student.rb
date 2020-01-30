8c30eef32ad19e5c8fcea002c0fe7e6a8bf8004a
Douglas Urner <dlu@canishe.com>
2020-01-30 15:14:42 -0800

    Handle nil and empty preferred_name, edit students for section

--- classstatus/app/models/student.rb
+++ classstatus/app/models/student.rb
@@ -10,15 +10,19 @@ class Student < ApplicationRecord
   end
 
   def display_name
-    "#{preferred_name ? preferred_name : given_name} #{family_name}"
+    "#{preferred_or_given_name} #{family_name}"
   end
 
   def initials
-    "#{preferred_name ? preferred_name[0] : given_name[0]}#{family_name[0]}"
+    "#{preferred_name.blank? ? preferred_name[0] : given_name[0]}#{family_name[0]}"
   end
 
   def sortable_name
-    "#{family_name}, #{preferred_name ? preferred_name : given_name}"
+    "#{family_name}, #{preferred_name.blank? ? preferred_name : given_name}"
+  end
+
+  def preferred_or_given_name
+    "#{preferred_name.blank? ? given_name : preferred_name}"
   end
 
   def section_list
