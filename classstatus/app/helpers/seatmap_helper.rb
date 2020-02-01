813e616eb10e3ed178faf87554f92498f045ba71
Douglas Urner <dlu@canishe.com>
2020-01-31 17:55:59 -0800

    As used on day 1

--- classstatus/app/helpers/seatmap_helper.rb
+++ classstatus/app/helpers/seatmap_helper.rb
@@ -4,7 +4,11 @@ module SeatmapHelper
 
     html = "<div class='area unseated'>\n"
     @enrollments.each do |e|
-      html += student_item(e)
+      if (action_name == 'attendance')
+        html += student_item_varient(e)
+      else
+        html += student_item(e)
+      end
     end
     (total_seats - @enrollments.length).times do
       html += empty_seat
@@ -36,7 +40,17 @@ module SeatmapHelper
   end
 
   def seatmap(rows, cols, row_lengths)
-    html = "<div class='seatmap'>\n"
+    html = "<div class='seatmap' style='width: 100%'>\n"
+    rows.times do |r|
+      html += seatmap_row(r, cols, row_lengths[r])
+    end
+    html += "</div>\n"
+
+    html.html_safe
+  end
+
+  def seatmap_variant(rows, cols, row_lengths)
+    html = "<div class='seatmap name-only' style='width: 100%'>\n"
     rows.times do |r|
       html += seatmap_row(r, cols, row_lengths[r])
     end
@@ -65,14 +79,96 @@ module SeatmapHelper
   end
 
   ###
-  # Generate one student tile.
+  # Generate the container for one student tile.
   # @type {Enrollment}
   #
   def student_item(e)
-    html =  "<div class='student-item' data-student-id='#{e.student_id}'>\n"
+    html  = "<div class='student-item' data-student-id='#{e.student_id}'>\n"
     html += "<div class='student-content' data-student-id='#{e.student_id}'>\n"
+    html += student_tile(e)
+    html += "</div>\n"
+    html += "</div>\n"
+  end
+
+  def student_item_varient(e)
+    html  = "<div class='student-item' data-student-id='#{e.student_id}'>\n"
+    html += "<div class='student-content' data-student-id='#{e.student_id}'>\n"
+    html += student_tile_varient(e)
+    html += "</div>\n"
+    html += "</div>\n"
+  end
+
+  ###
+  # Generate a student tile
+  #
+  def student_tile(e)
+    html  = "<div class='student-tile'>\n"
+
+    html += "<div class='student-identify'>\n"
     html += "#{e.student.display_name}\n"
     html += "</div>\n"
+
+    html += "<div class='student-progress'>\n"
+    html += annunciator_bar(e)
+    html += "</div>\n"
+
+    html += "</div>\n"
+  end
+
+  def student_tile_varient(e)
+    html  = "<div class='student-tile'>\n"
+
+    html += "<div class='student-identify'>\n"
+    html += "#{e.student.display_name}\n"
+    html += "</div>\n"
+
+    html += "<div class='student-progress'>\n"
+    # html += annunciator_bar(e)
+    html += "</div>\n"
+
+    html += "</div>\n"
+  end
+
+  def annunciator_bar(e)
+    html  = "<div class='annunciator-bar'>\n"
+    html += annunciator_cohort(e)
+    html += annunciator_gpa(e)
+    html += annunciator_spare
+    html += annunciator_spare
+    html += annunciator_spare
+    html += "</div>\n"
+  end
+
+  def annunciator_gpa(e)
+    klass = 'annunciator annunciator-gpa'
+    color = 'pink'
+    color = case e.student.gpa
+    when 3.5..4.0 ; 'blue'
+    when 2.5..3.5 ; 'green'
+    when 1.5..2.5 ; 'yellow'
+    when 1.0..1.5 ; 'orange'
+    when 0.0..1.0 ; 'red'
+    end
+    if (e.student.gpa == 0 && e.student.cohort == 2023)
+      color = 'purple'
+    end
+    gpa_tag = "background-color: #{color}"
+    html  = "<div class=\'#{klass}\' style=\'#{gpa_tag}\'>\n"
+    html += "&nbsp;"
+    html += "</div>\n"
+  end
+
+  def annunciator_cohort(e)
+    # TODO: sanity check on calculations.
+    year = Date.current.year
+    html  = "<div class='annunciator annunciator-cohort'>\n"
+    html += "<div>#{e.student.cohort - year}</div>"
+    html += "</div>\n"
+  end
+
+  def annunciator_spare
+    html  = "<div class='annunciator annunciator-spare'>\n"
+    html += "&nbsp;"
     html += "</div>\n"
   end
 
