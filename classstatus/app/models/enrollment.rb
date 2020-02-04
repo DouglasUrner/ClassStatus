1d515e49e2ba5081792e983c9257137ab050cde3
Douglas Urner <dlu@canishe.com>
2020-02-03 19:40:18 -0800

    Predicates

--- classstatus/app/models/enrollment.rb
+++ classstatus/app/models/enrollment.rb
@@ -5,6 +5,25 @@ class Enrollment < ApplicationRecord
   # TODO: validations on joined and dropped dates.
 
   def state
-    self.dropped_course ? 'Dropped' : 'Active'
+    self.dropped? ? 'Dropped' : 'Active'
+  end
+
+  def active?
+    if (self.joined_course >= self.section.term.start_date &&
+        self.joined_course <= self.section.term.end_date &&
+        self.dropped? == false
+    )
+      true
+    else
+      false
+    end
+  end
+
+  def dropped?
+    self.dropped_course ? true : false
+  end
+
+  def drop!(date = Date.today)
+    self.update({dropped_course: date})
   end
 end
