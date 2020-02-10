906c515e43d3a9d237426fbc45827421f4327a47
Douglas Urner <dlu@canishe.com>
2020-02-09 21:23:28 -0800

    Update section_test.rb

--- classstatus/test/models/section_test.rb
+++ classstatus/test/models/section_test.rb
@@ -4,13 +4,24 @@ class SectionTest < ActiveSupport::TestCase
   def setup
     @section = sections(:one)
   end
-  
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should get academic year for section' do
+    year_name = @section.year_name
+    assert_equal '1', year_name
+  end
+
+  test 'should get block name for section' do
+    block_name = @section.block_name
+    assert_equal 'A1', block_name
+  end
 
   test 'should get course name for section' do
-    section_name = @section.section_name
-    assert_equal section_name, 'Section One'
+    course_name = @section.course_name
+    assert_equal 'Course One', course_name
+  end
+
+  test 'should get term name for section' do
+    term_name = @section.term_name
+    assert_equal 'Term 1', term_name
   end
 end
