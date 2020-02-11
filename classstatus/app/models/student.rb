720439501b4d98ed11f113bad04f15f99b79972f
Douglas Urner <dlu@canishe.com>
2020-02-10 23:06:15 -0800

    Fixtures & tests for Student model

--- classstatus/app/models/student.rb
+++ classstatus/app/models/student.rb
@@ -1,2 +1,25 @@
 class Student < ApplicationRecord
+  def name
+    self.full_name
+  end
+
+  def full_name
+    "#{self.preferred_or_given_name} #{self.family_name}"
+  end
+
+  def sortable_name
+    "#{self.family_name}, #{self.preferred_or_given_name}"
+  end
+
+  def short_name
+    "#{self.preferred_or_given_name} #{self.family_name[0]}."
+  end
+
+  def initials
+    "#{self.preferred_or_given_name[0]}#{self.family_name[0]}"
+  end
+
+  def preferred_or_given_name
+    self.preferred_name ? self.preferred_name : self.given_name
+  end
 end
--- classstatus/test/models/student_test.rb
+++ classstatus/test/models/student_test.rb
@@ -4,4 +4,33 @@ class StudentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @student1 = students(:one)
+    @student2 = students(:two)
+  end
+
+  test 'name should be full name' do
+    assert_equal @student1.name, @student1.full_name
+  end
+
+  test 'should generate full name' do
+    assert_equal 'Given1 Family1', @student1.full_name
+  end
+
+  test 'should generate sortable name' do
+    assert_equal 'Family1, Given1', @student1.sortable_name
+  end
+
+  test 'should generate short name' do
+    assert_equal 'Given1 F.', @student1.short_name
+  end
+
+  test 'should generate initials' do
+    assert_equal 'GF', @student1.initials
+  end
+
+  test 'should choose preferred name over given name' do
+    assert_equal 'Preferred2', @student2.preferred_or_given_name
+  end
 end
