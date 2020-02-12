3dd9b79ce3aae207b7ceb1613aafa2b101b67bae
Douglas Urner <dlu@canishe.com>
2020-02-12 12:23:06 -0800

    Updates tests & fixtures

--- classstatus/test/models/section_test.rb
+++ classstatus/test/models/section_test.rb
@@ -7,7 +7,7 @@ class SectionTest < ActiveSupport::TestCase
 
   test 'should get academic year for section' do
     year_name = @section.year_name
-    assert_equal '1', year_name
+    assert_equal '2019-2020', year_name
   end
 
   test 'should get block name for section' do
@@ -25,15 +25,19 @@ class SectionTest < ActiveSupport::TestCase
     assert_equal 'Term 1', term_name
   end
 
+  test 'should get term short name for section' do
+    assert_equal 'T1', @section.term_short_name
+  end
+
   test 'name should be the short name of the section' do
     assert_equal @section.short_name, @section.name
   end
 
   test 'should get long name for section' do
-    assert_equal 'Course One ()', @section.long_name
+    assert_equal 'A1: Course One (T1 - 2019-2020)', @section.long_name
   end
 
   test 'should get the short name for the section' do
-    assert_equal 'Course One (A1)', @section.short_name
+    assert_equal 'A1: Course One', @section.short_name
   end
 end
